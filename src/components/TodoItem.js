70f24dde9b75fa833cb5707ef6a17f499b6c843c
Alexandr <alex.zakharov2802@gmail.com>
2020-11-02 11:16:48 +0300

    Added the ability to complete todo items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,7 @@ const TodoItem = (props) => {
   return (
     <div className={props.item.isCompleted ? 'todo completed': 'todo'} >
       <li className="todo-item">{props.item.text}</li>
-      <button className="complete-btn">
+      <button className="complete-btn" onClick={() => props.completeTodoItem(props.item.id)}>
         <i className="fas fa-check"></i>
       </button>
       <button className="trash-btn" onClick={() => props.removeTodoItem(props.item.id)}>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -13,11 +13,29 @@ const TodoList = (props) => {
         }),
     ]);
   };
+
+  const completeTodoItem = (id) => {
+    props.setTodoItems([
+      ...props.todoItems.map((item, i) => {
+        return i === id
+          ? {
+              ...item,
+              isCompleted: !item.isCompleted,
+            }
+          : item;
+      }),
+    ]);
+  };
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {props.todoItems.map((item) => (
-          <TodoItem key={item.id} item={item} removeTodoItem={removeTodoItem} />
+          <TodoItem
+            key={item.id}
+            item={item}
+            removeTodoItem={removeTodoItem}
+            completeTodoItem={completeTodoItem}
+          />
         ))}
       </ul>
     </div>
